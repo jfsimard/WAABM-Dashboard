ad29164af1a8b734a02604981490fe8339077391
JF Simard <jfsimard2@gmail.com>
2024-09-16 13:21:46 -0400

    Added Dark theme

---
+++ js/script.js
@@ -0,0 +1,8 @@
+const bdy = document.getElementById('dashboard');
+const switchBtn = document.getElementById('switch');
+switchBtn.addEventListener('click', changeTheme)
+
+function changeTheme() {
+    bdy.classList.toggle("dark-theme");
+    switchBtn.classList.toggle("dark");
+}
